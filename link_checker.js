5b4c689dfe655c9256269500483433ed4a2537a5
Brian Immel <jawa9000@gmail.com>
2019-09-11 11:23:15 -0700

    link_checker clean up and search for broken links

--- link_checker.js
+++ link_checker.js
@@ -2,25 +2,28 @@ var fs = require('fs');
 var cheerio = require('cheerio');
 var shell = require('shelljs');
 var util = require('util');
-
 var links = {}; // object to hold the page name and
+
 shell.ls('htmlguides/*.html').map(function(file) {
   $ = cheerio.load(fs.readFileSync(file).toString());
-  console.log('\nChecking links in ' + file);
+  console.log('Checking links in ' + file);
 
   links[file] = file;
 
   if ($('a[href]')) { // if there is an link in the file...
     links[file] = {};
     var array = [];
-    $('a[href]').each( (index, value) => {
+
+    $('a[href]').each((index, value) => {
       if ($(value).attr('href').indexOf('http') == -1) { // add http path cuz it's most likely an Appc doc
         var url = 'docs.appcelerator.com/platform/latest/#!/guide/' + $(value).attr('href');
       } else {
-        var url = $(value).attr('href')
+        var url = $(value).attr('href');
       }
+
       array.push(url);
     });
+
     links[file].urls = array;
   }
 });
