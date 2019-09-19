9f6583df1a19b49c099e188d0537a9eeb532a32f
Brian Immel <jawa9000@gmail.com>
2019-09-19 08:10:29 -0700

    Update push_zoomin_bundle.js
    Finished bundle mappings, added routine to figure out server locations, revised bundle routine to handle spaces between commas, updated bundle messaging, started testing issue with permission denied, and added a function to handle messaging and exiting of app.

--- push_zoomin_bundle.js
+++ push_zoomin_bundle.js
@@ -3,21 +3,28 @@
     Usage: node push_zoomin_bundle.js -s <server> -b "<bundle(s)>" -k <path/to/RSA_key>
     Documentation: https://wiki.appcelerator.org/x/rsKVAw
 */
+
 const program = require('commander');
-var SftpUpload = require('sftp-upload');
-var fs = require('fs');
+const SftpUpload = require('sftp-upload');
+const fs = require('fs');
+const flags = ['server','bundle','path','key']; // list of available flags
+const bundleName = ['api builder','api runtime services','api runtime services self','alloy','amplify cli','amplify dashboard','amplify services','amplify services overview','appc cli','appc dashboard','studio','mbs','sphere','syncplicity','sdk']; // names of the various products or top-level documents
+const bundleDirectory = ['API_Builder','API_Runtime_Services','API_Runtime_Services_self-install','Alloy','Amplify_CLI','Amplify_Dashboard','Amplify_Services','Amplify_Services_Overview','Appcelerator_CLI','Appcelerator_Dashboard','Appcelerator_Studio','Mobile_Backend_Services','Sphere','Syncplicity','Titanium_SDK']; // bundle directory end path
+var message = ''; // variable to hold the display message for bundle attempts and to which servers
 var arguments = {}; // object to hold the entered arguments
+var bundleList = []; // array to hold one or more bundle names
+var bundleListPaths = []; // array to hold the path(s) of one or more bundle paths
+var bundleCount = 0; // used to confirm that the right number of bundles are being sent
+var serverCount = 0; // variable to track number of properly submitted server names
 
 program // set up flags
-  .version('0.0.2')
+  .version('0.1.0')
   .usage('node push_zoomin_bundle.js -s <server> -b "<bundle(s)>" -k <path/to/RSA_key> -p <non-default/path/to/bundles>')
   .option('-s, --server', '*Required* Which server to update the bundle to. Options: "sandbox" or "prod".')
   .option('-b, --bundle', '*Required* Which bundle to upload.')
   .option('-p, --path', 'Which path to use for your bundle.')
   .option('-k, --key', '*Required* Path and filename of the RSA key.')
   .parse(process.argv);
-
-var flags = ['server','bundle','path','key']; // list of available flags
 
 for (var i = 0; i < program.rawArgs.length; i++) { // loop through the array of raw arguments to gather the entered arguments
     for (j in flags) { // loop through flags array, determine if the flag was set, and add the value to the arguments object
@@ -28,31 +35,43 @@ for (var i = 0; i < program.rawArgs.length; i++) { // loop through the array of 
 }
 
 if (arguments.server) { // set up pathway for Zoomin server
-    if (arguments.server == 'sandbox') {
-        arguments.server = '/axway.zoominsoftware.io/confluence/incoming';
-    } else if (arguments.server == 'prod') {
-        arguments.server = '/docs.axway.com/confluence/incoming';
+    if (arguments.server.indexOf(',') > -1 || arguments.server.indexOf(', ') > -1) { // check if the request is for a single server or multiple
+        var servers = arguments.server.replace(/, /g, ',').split(','); // split argument into an array
+
+        for (i in servers) { // check if the submitted server names check out
+            if (servers[i] == 'prod') {
+                servers[i] = '/docs.axway.com/confluence/incoming'; // re-assign array value to proper server path
+                serverCount++;
+            } else if (servers[i] == 'sandbox') {
+                servers[i] = '/axway.zoominsoftware.io/confluence/incoming'; // re-assign array value to proper server path
+                serverCount++;
+            }
+        }
+
+        if (serverCount == 2) { // if the server names check out, update display message
+            message = ' to both the sandbox and prod servers.';
+        } else { // if not, exit program, program
+            messageExit('Check spelling of submitted server names. Select either "sandbox" or "prod".\nEnd of line.', true);
+        }
+    } else { // assume the upload attempt is to a single server
+        if (arguments.server == 'sandbox' || arguments.server == 'prod') { // check if server name checks out
+            var servers = arguments.server;
+            message = ' to the ' + servers + ' server.';
+        } else {
+            messageExit('Server is required. Select either "sandbox" or "prod".\nEnd of line.', true);
+        }
     }
 } else {
-    console.log('Server is required. Select either "sandbox" or "prod". End of line.');
-
-    process.exit(1);  
+    messageExit('Server is required. Select either "sandbox" or "prod".\nEnd of line.', true);
 }
 
 if (!arguments.path) { // set default path for product bundles
     arguments.path = '/Users/bimmel/Documents/Repositories/zoomin/products/Bundles/'; // default path to individual bundles
 }
 
-const bundleName = ['api builder','api runtime services','api runtime services self','alloy','amplify cli','amplify dashboard','amplify services','amplify services overview','appc cli','appc dashboard','studio','mbs','sphere','syncplicity','sdk']; // names of the various products or top-level documents
-const bundleDirectory = ['API_Builder','API_Runtime_Services','API_Runtime_Services_self-install','Alloy','Amplify_CLI','Amplify_Dashboard','Amplify_Services','Amplify_Services_Overview','Appcelerator_CLI','Appcelerator_Dashboard','Appcelerator_Studio','Mobile_Backend_Services','Sphere','Syncplicity','Titanium_SDK']; // bundle directory end path
-
-if (arguments.bundle) { // set up pathway for bundle to be uploaded
-    var bundleList = []; // array to hold one or more bundle names
-    var bundleListPaths = []; // array to hold the path(s) of one or more bundle paths
-    var bundleCount = 0; // used to confirm that the right number of bundles are being sent
-    
-    if (arguments.bundle.indexOf(',') > -1) { // if there is a comma separated, assume this is a multiple bundle upload request
-        bundleList = arguments.bundle.split(',');
+if (arguments.bundle) { // set up pathway for bundle to be uploaded    
+    if (arguments.bundle.indexOf(',') > -1 || arguments.bundle.indexOf(', ') > -1) { // if there is a comma separated, assume this is a multiple bundle upload request
+        bundleList = arguments.bundle.replace(/, /g, ',').split(','); // remove any spaces after the comma and convert into an array
     } else { // process a single bundle
         bundleList.push(arguments.bundle);
     }
@@ -67,52 +86,68 @@ if (arguments.bundle) { // set up pathway for bundle to be uploaded
     }
 
     if (bundleCount == bundleList.length) { // if the bundleList length doesn't match the bundleCount, then one or more of the 
-        console.log('Attempting to upload ' + bundleCount + ' bundles.');
+        if (bundleCount == 1) {
+            message = 'Attempting to upload one bundle' + message;
+        } else {
+            message = 'Attempting to upload ' + bundleCount + ' bundles' + message;
+        }
+        messageExit(message, false);
     } else {
-        console.log('One of the bundles isn\'t right. Please use one of the following:');
+        message = 'One of the bundle names isn\'t right. Please use one of the following: ';
         
-        for (i in bundleName) {
-            console.log(bundleName[i]);
+        for (var i = 0; i < bundleName.length; i++) {
+            if (i == bundleName.length - 1) {
+                message += 'and/or "' + bundleName[i] + '".\n';
+            } else {
+                message += '"' + bundleName[i] + '", ';
+            }
         }
 
-        console.log('End of line.');
-
-        process.exit(1); 
+        messageExit(message + 'End of line.', true);
     }
 }
 
 if (!arguments.key) {
-    console.log('A RSA key is required. End of line.');
-
-    process.exit(1);
+    messageExit('A RSA key is required. End of line.', true);
 }
 
-if (arguments.server && arguments.path && arguments.bundle && arguments.key) {
-    for (i in bundleListPaths) { // loop through all bundles and sftp them to the zoomin server
-        var options = { // set options for SFTP upload
-            host: 'upload.zoominsoftware.io', // zoomin host
-            username: 'axway', // our username
-            path: bundleListPaths[i], // path to the bundle to be uploaded
-            remoteDir: arguments.server, // path to the server
-            privateKey: fs.readFileSync(arguments.key) // RSA key
+if (arguments.server && arguments.path && arguments.bundle && arguments.key) { // if all required arguments are present, push the bundles to the Zoomin server
+    for (i in servers) {
+        for (k in bundleListPaths) { // loop through all bundles and sftp them to the zoomin server
+            var options = { // set options for SFTP upload
+                host: 'upload.zoominsoftware.io', // zoomin host
+                username: 'axway', // our username
+                path: bundleListPaths[k], // path to the bundle to be uploaded
+                remoteDir: servers[i], // path to the server
+                privateKey: fs.readFileSync(arguments.key) // RSA key
+            }
+
+            console.log(options)
+    
+            // sftp = new SftpUpload(options);
+    
+            // sftp.on('error', function(err) {
+            //     throw err;
+            //     })
+            //     .on('uploading', function(progress) {
+            //         console.log('Uploading', progress.file);
+            //         console.log(progress.percent + '% completed');
+            //     })
+            //     .on('completed', function() {
+            //         console.log('Upload complete.');
+            //     })
+            //     .upload();
         }
-
-        sftp = new SftpUpload(options);
-
-        sftp.on('error', function(err) {
-            throw err;
-            })
-            .on('uploading', function(progress) {
-                console.log('Uploading', progress.file);
-                console.log(progress.percent + '% completed');
-            })
-            .on('completed', function() {
-                console.log('Upload complete.');
-            })
-            .upload();
     }
+    
 } else { // catch any missing required flags
-    console.log('You are missing one or more arguments. End of line.');
+    messageExit('You are missing one or more arguments.\nEnd of line.', true);
+}
 
-    process.exit(1);
+function messageExit(message, exit) { // display message(s) and exit the program if necessary
+    console.log(message);
+
+    if (exit) { // if the MCU requests, and exit, terminate the program
+        process.exit(1);
+    }
 }
